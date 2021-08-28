89ed7006642dc60bc6e1f58a98773dd1a3c49444
MalcolmVane <69203541+MalcolmVane@users.noreply.github.com>
2021-08-28 16:03:24 -0400

    Update database.js

--- database.js
+++ database.js
@@ -75,6 +75,419 @@ const organisms = [
     description: ``,
     wikipedia: 'https://en.m.wikipedia.org/wiki/Smooth_green_snake',
     keywords: 'seed;'
-  }
+  },
+  {
+    title: 'American Bullfrog',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/North-American-bullfrog1.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/American_bullfrog',
+    keywords: 'seed;'
+  },
+  {
+    title: 'Northern Leopard Frog',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Lithobates_pipiens.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Northern_leopard_frog',
+    keywords: 'seed;'
+  },
+  {
+    title: 'American Toad',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/0/0f/Bufo_americanus_PJC1.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/American_toad',
+    keywords: 'seed;'
+  },
+  {
+    title: 'Pickerel Frog',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Pickerel_Frog.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Pickerel_frog',
+    keywords: 'seed;'
+  },
+  {
+    title: 'Wood Frog',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/76/Lithobates_sylvaticus_%28Woodfrog%29.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Wood_frog',
+    keywords: 'seed;'
+  },
+  {
+    title: 'Gray Treefrog',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/76/Gray_treefrog_%28Hyla_versicolor%29.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Gray_treefrog',
+    keywords: 'seed;'
+  },
+  {
+    title: 'Spotted Salamander',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/8/84/Spotted_Salamander%2C_Cantley%2C_Quebec.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Spotted_salamander',
+    keywords: 'seed;chordate;amphibian;'
+  },
+  {
+    title: 'Broadhead Skink',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Eumeces_laticeps_%28male%29.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Plestiodon_laticeps',
+    keywords: 'seed;chordate;reptile;'
+  },
+  {
+    title: 'Five-lined Skink',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/c/c4/Eumeces_fasciatusPCCA20040425-1563A.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Plestiodon_fasciatus',
+    keywords: 'seed;chordate;reptile;'
+  },
+  {
+    title: 'Texas horned lizard',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/68/TexasHornedLizard.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Texas_horned_lizard',
+    keywords: 'seed;'
+  },
+  {
+    title: 'Eastern Glass Lizard',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/4/4d/Eastern_Glass_Lizard.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Eastern_glass_lizard',
+    keywords: 'seed;chordate;reptile;'
+  },
+  {
+    title: 'American Alligator',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/0/03/American_Alligator.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/American_alligator',
+    keywords: 'seed;chordate;reptile'
+  },
+  {
+    title: 'Alligator Snapping turtle',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Alligator_snapping_turtle.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Alligator_snapping_turtle',
+    keywords: 'seed;chordate;reptile;'
+  },
+  {
+    title: 'Razor-Backed Musk Turtle',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/2/2a/RazorBackMuskTurtle.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Razor-backed_musk_turtle',
+    keywords: 'seed;chordate;reptile;'
+  },
+  {
+    title: 'Various chickadees',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/4/4a/Poecile-atricapilla-001.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Chickadee',
+    keywords: ''
+  },
+  {
+    title: 'House Sparrow',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/6e/Passer_domesticus_male_%2815%29.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/House_sparrow',
+    keywords: ''
+  },
+  {
+    title: 'Bohemian Waxwing',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Bombycilla_garrulusII.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Bohemian_waxwing',
+    keywords: ''
+  },
+  {
+    title: 'Mallard',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Anas_platyrhynchos_male_female_quadrat.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Mallard',
+    keywords: ''
+  },
+  {
+    title: 'Muscovy Duck',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/MuscovyDuck.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Muscovy_duck',
+    keywords: ''
+  },
+  {
+    title: 'Wood Duck',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Wood_Duck_%28Aix_sponsa%29%2C_Parc_du_Rouge-Clo%C3%AEtre%2C_Brussels.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Wood_duck',
+    keywords: ''
+  },
+  {
+    title: 'Bald Eagle',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/1a/About_to_Launch_%2826075320352%29.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Bald_eagle',
+    keywords: ''
+  },
+  {
+    title: 'Canadian Goose',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/4/40/Canada_goose_on_Seedskadee_NWR_%2827826185489%29.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Canada_goose',
+    keywords: ''
+  },
+  {
+    title: 'Snowy Owl',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/6d/Snowy_Owl_%28240866707%29.jpeg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Snowy_owl',
+    keywords: ''
+  },
+  {
+    title: 'Grey-beacked Shrike',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/68/Grey-backed_Shrike_Khangchendzonga_Biosphere_Reserve_West_Sikkim_India_21.02.2016.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Grey-backed_shrike',
+    keywords: ''
+  },
+  {
+    title: 'Loggerhead Shrike',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/73/Lanius_ludovicianus_-Texas_-USA-8-4c.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Loggerhead_shrike',
+    keywords: ''
+  },
+  {
+    title: 'Common Raven',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/30/Corvus_corax.001_-_Tower_of_London.JPG',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Common_raven',
+    keywords: ''
+  },
+  {
+    title: 'Wild Turkey',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/5/50/Gall-dindi.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Wild_turkey',
+    keywords: ''
+  },
+  {
+    title: 'Harris Hawk',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/65/Harris%27s_Hawk_%28Parabuteo_unicinctus%29_3_of_4_in_set.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Harris%27s_hawk',
+    keywords: ''
+  },
+  {
+    title: 'California Condor',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Gymnogyps_californianus_-San_Diego_Zoo-8a.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/California_condor',
+    keywords: ''
+  },
+  {
+    title: 'Spruce Grouse',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/0/0a/Falcipennis-canadensis-002.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Spruce_grouse',
+    keywords: ''
+  },
+  {
+    title: 'Tufted Titmouse',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/6e/Baeolophus_bicolor_15.jpeg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Tufted_titmouse',
+    keywords: ''
+  },
+  {
+    title: 'Gray Jay',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/Perisoreus_canadensis_mercier2.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Canada_jay',
+    keywords: ''
+  },
+  {
+    title: 'Black-Tailed Jackrabbit',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/6d/Jackrabbit2_crop.JPG',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Black-tailed_jackrabbit',
+    keywords: ''
+  },
+  {
+    title: 'Raccoon',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Raccoon_climbing_in_tree_-_Cropped_and_color_corrected.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Raccoon',
+    keywords: ''
+  },
+  {
+    title: 'Peccary',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Collared_peccary02_-_melbourne_zoo.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Peccary',
+    keywords: ''
+  },
+  {
+    title: 'Mule Deer',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/39/Mule_deer_buck_full-face.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Mule_deer',
+    keywords: ''
+  },
+  {
+    title: 'White Tailed Deer',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Odocoileus_virginianus_%28white-tailed_deer_-_buck_in_velvet%29_%2817_July_2018%29_%28Newark%2C_Ohio%2C_USA%29_3_%2829623674898%29.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/White-tailed_deer',
+    keywords: ''
+  },
+  {
+    title: 'Moose',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Moose_superior.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Moose',
+    keywords: ''
+  },
+  {
+    title: 'Grey Wolf',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Kolm%C3%A5rden_Wolf.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Wolf',
+    keywords: ''
+  },
+  {
+    title: 'Coyote',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/9c/2009-Coyote-Yosemite.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Coyote',
+    keywords: ''
+  },
+  {
+    title: 'American Mink',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/0/04/MinkforWiki.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/American_mink',
+    keywords: ''
+  },
+  {
+    title: 'Reindeer',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/96/Reinbukken_p%C3%A5_frisk_gr%C3%B8nt_beite._-_panoramio.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Reindeer',
+    keywords: ''
+  },
+  {
+    title: 'Brown Bear',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/71/2010-kodiak-bear-1.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Brown_bear',
+    keywords: ''
+  },
+  {
+    title: 'Swamp Rabbit',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/c/c1/Swamp_Rabbit_%28Sylvilagus_aquaticus%29.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Swamp_rabbit',
+    keywords: ''
+  },
+  {
+    title: 'American Bison',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/8/8d/American_bison_k5680-1.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/American_bison',
+    keywords: ''
+  },
+  {
+    title: 'Nine-banded Armadillo',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/e/e3/Florida-015.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Nine-banded_armadillo',
+    keywords: ''
+  },
+  {
+    title: 'Virginia Opossum',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/2/27/Opossum_2.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Virginia_opossum',
+    keywords: ''
+  },
+  {
+    title: 'Pronghorn',
+    image: 'https://en.m.wikipedia.org/wiki/File:Antilocapra_americana.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Pronghorn',
+    keywords: ''
+  },
+  {
+    title: 'Tricolored Bat',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Perimyotis_subflavus_picture_cropped.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Tricolored_bat',
+    keywords: ''
+  },
+  {
+    title: 'North American Beaver',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/6b/American_Beaver.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/North_American_beaver',
+    keywords: ''
+  },
+  {
+    title: 'Eastern Grey Squirrel',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/0/0a/Eastern_Grey_Squirrel.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Eastern_gray_squirrel',
+    keywords: ''
+  },
+  {
+    title: 'Eastern Mole',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/3e/ScalopusAquaticus.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Eastern_mole',
+    keywords: ''
+  },
+  {
+    title: 'Bighorn Sheep',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/97/New_Mexico_Bighorn_Sheep.JPG',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Bighorn_sheep',
+    keywords: ''
+  },
+  {
+    title: 'Deer Mouse',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/33/DiGangi-Deermouse.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Peromyscus',
+    keywords: ''
+  },
+  {
+    title: 'Gophers',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Pocket-Gopher_Ano-Nuevo-SP.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Gopher',
+    keywords: ''
+  },
+  {
+    title: 'Groundhogs',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/93/Marmota_monax_UL_04.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Groundhog',
+    keywords: ''
+  },
+  {
+    title: 'North American Ground Lemming',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Tunturisopuli_Lemmus_Lemmus.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Lemming',
+    keywords: ''
+  },
+  {
+    title: 'Red Fox',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/16/Fox_-_British_Wildlife_Centre_%2817429406401%29.jpg',
+    description: ``,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Red_fox',
+    keywords: ''
+  },
+  {
+    title: 'Ring Billed Gull',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Ring-billed_gull_in_Red_Hook_%2842799%29.jpg',
+    description: `<p style="color:red;"><b>REDACTED</b></p>`,
+    wikipedia: 'https://en.m.wikipedia.org/wiki/Ring-billed_gull',
+    keywords: 'chordates;seed;unintentional seed;seagull;conspiracy;bird;'
+  },
 ];
 
